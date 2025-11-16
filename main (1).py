ce51918588780b8f0af31cd906f8af261218113d
izxnxx <vladyslav.hresko.kb.2025@lpnu.ua>
2025-11-16 19:22:24 +0200

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,13 @@
+from scanner import load_rules, scan_file
+
+rules_path = "rules.txt"
+file_to_scan = "test_files/infected_file.txt"  # поміняй на реальний файл
+
+rules = load_rules(rules_path)
+
+found = scan_file(file_to_scan, rules)
+
+if found:
+    print(f"УВАГА! У файлі {file_to_scan} знайдено загрози: {found}")
+else:
+    print(f"Файл {file_to_scan} чистий.")
---
+++ scanner.py
@@ -0,0 +1,14 @@
+def load_rules(filepath):
+
+    with open(filepath, "r", encoding="utf-8") as f:
+        rules = [line.strip() for line in f if line.strip()]
+    return rules
+
+def scan_file(filepath, rules):
+    found = []
+    with open(filepath, "r", encoding="utf-8") as f:
+        content = f.read()
+        for rule in rules:
+            if rule in content:
+                found.append(rule)
+    return found
