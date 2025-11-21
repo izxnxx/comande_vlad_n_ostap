0149e4e62eae72429b180721f6852907975635a1
izxnxx <vladyslav.hresko.kb.2025@lpnu.ua>
2025-11-21 10:10:52 +0200

    main.py

--- main (1).py
+++ main (1).py
@@ -1,7 +1,7 @@
 from scanner import load_rules, scan_file
 
 rules_path = "rules.txt"
-file_to_scan = "test_files/infected_file.txt"  
+file_to_scan = "rules.txt"  
 
 rules = load_rules(rules_path)
 
@@ -11,4 +11,5 @@ if found:
     print(f"УВАГА! У файлі {file_to_scan} знайдено загрози: {found}")
 else:
     print(f"Файл {file_to_scan} чистий.")
+
 
